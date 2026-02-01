c6b6b065882bb004696dc1d6b93b4ff9d72b9baa
Dambar <dambarojha440@gmail.com>
2026-02-01 14:44:32 -0800

    added form subbmit button

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,37 +1,40 @@
-/*---------Random Codes---------*/
-//Function to generate combination of characters
+var code = "";
+var getCode = '';
+var btnValue;
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+
 function generateCode() {
 
-}
-
-//Function to generate combination of characters
-function generateCode() {
-    //Create variable to store generated codes and the types of characters we want to show as codes
-    var code= ''; //Initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-}
-
-//Generate characters multiple times using a loop
-for (i =1; i <= 8; i++) {
-
- var char = Math.random() * str.length;
+  for (var i = 1; i <= 8; i++) {
+    var char = Math.random() * str.length;
     code += str.charAt(char);
   }
 
   return code;
-
+}
 
 document.getElementById("codes").innerHTML = generateCode();
 
 function disableButton(btnValue) {
-    document.getElementById("submit").disabled = true;
-
+    document.getElementById("submit").disabled = btnValue;
+    if (btnValue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-//Disable button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
-}
+var codebox = document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
 
-//Activate function
-disableButton();
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false);
+    }
+}
